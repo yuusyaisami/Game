c2913fb0635db08360cc882747837dd37a6970e7
Yuisami <macaronkunkun@gmail.com>
2024-04-22 23:51:40 +0900

    #

--- Controller.py
+++ Controller.py
@@ -2,10 +2,15 @@ import pygame
 class Controller:
     def __init__(self, ):
         self.event = None
-        self.stute = {"w": False, "a": False, "s": False, "d": False, "esc": False, "space": False, "enter": False, "tab": False, "backspace": False}
+        self.stute = {"w": False, "a": False, "s": False, "d": False, "esc": False, 
+                      "space": False, "enter": False, "tab": False, "backspace": False,
+                      "click": False, "click_right": False, "click_left": False,
+                      }
     def update(self, event):
         self.event = event
-        stute = {"w": False, "a": False, "s": False, "d": False, "esc": False, "space": False, "enter": False, "tab": False, "backspace": False}
+        stute = {"w": False, "a": False, "s": False, "d": False, "esc": False,
+                "space": False, "enter": False, "tab": False, "backspace": False,
+                 "click": False, "click_right": False, "click_left": False,}
         for event in self.event:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:
@@ -28,28 +33,34 @@ class Controller:
                     stute["backspace"] = True
     def get_keydown(self):
         """ keyname is a string representing"""
-        stute = {"w": False, "a": False, "s": False, "d": False, "esc": False, "space": False, "enter": False, "tab": False, "backspace": False}
+        self.stute = {"w": False, "a": False, "s": False, "d": False, "esc": False, "space": False, "enter": False, "tab": False, "backspace": False}
         for event in self.event:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w:
-                    stute["w"] = True
+                    self.stute["w"] = True
                 elif event.key == pygame.K_s:
-                    stute["s"] = True
+                    self.stute["s"] = True
                 elif event.key == pygame.K_a:
-                    stute["a"] = True
+                    self.stute["a"] = True
                 elif event.key == pygame.K_d:
-                    stute["d"] = True
+                    self.stute["d"] = True
                 elif event.key == pygame.K_ESCAPE:
-                    stute["esc"] = True
+                    self.stute["esc"] = True
                 elif event.key == pygame.K_SPACE:
-                    stute["space"] = True
+                    self.stute["space"] = True
                 elif event.key == pygame.K_RETURN:
-                    stute["enter"] = True
+                    self.stute["enter"] = True
                 elif event.key == pygame.K_TAB:
-                    stute["tab"] = True
+                    self.stute["tab"] = True
                 elif event.key == pygame.K_BACKSPACE:
-                    stute["backspace"] = True
-        return stute
+                    self.stute["backspace"] = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                buttons = pygame.mouse.get_pressed()
+                if buttons[0]:
+                    self.stute["click_right"] == True
+                self.stute["click"] = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                self.stute["click_right"]
     def get_click(self, number = -1):
         if number == -1:
             for self.event in self.event
